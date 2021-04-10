c46763ec1779f21772498e5b26039f6954bb29eb
MacRat <m@crat.jp>
2021-04-10 20:25:22 +0900

    feat: change default status page to HTML version

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -13,6 +13,9 @@ var faviconIco []byte
 //go:embed static/favicon.svg
 var faviconSvg []byte
 
+//go:embed static/not-found.html
+var notFoundPage []byte
+
 func New(s *store.Store) http.Handler {
 	m := http.NewServeMux()
 
@@ -27,7 +30,14 @@ func New(s *store.Store) http.Handler {
 	m.HandleFunc("/status.html", HTMLExporter(s))
 	m.HandleFunc("/status.json", JSONExporter(s))
 
-	m.Handle("/", http.RedirectHandler("/status.txt", http.StatusFound))
+	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/" {
+			http.Redirect(w, r, "/status.html", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write(notFoundPage)
+		}
+	})
 
 	return m
 }
