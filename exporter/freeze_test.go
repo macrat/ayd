45a7175f053c4d47a4897908ac13e91eb394d36f
MacRat <m@crat.jp>
2021-04-21 22:09:35 +0900

    test: add test of freeze probe history

---
+++ exporter/freeze_test.go
@@ -0,0 +1,120 @@
+package exporter
+
+import (
+	"net/url"
+	"testing"
+	"time"
+	"reflect"
+
+	"github.com/macrat/ayd/store"
+)
+
+func TestFreezeProbeHistory(t *testing.T) {
+	filledRecords := []*store.Record{}
+	for i := 0; i < store.PROBE_HISTORY_LEN - 1; i++ {
+		filledRecords = append(filledRecords, &store.Record{
+			CheckedAt: time.Date(2021, time.January, 2, 15, 4, 5, 0, time.UTC),
+			Status: store.STATUS_HEALTHY,
+			Target: &url.URL{Scheme: "ping", Opaque: "local"},
+			Message: "filled",
+			Latency: 123456 * time.Microsecond,
+		})
+	}
+
+	tests := []struct{
+		Name        string
+		Records     []*store.Record
+		Updated     string
+		Status      string
+		FirstRecord frozenRecord
+		LastRecord  frozenRecord
+	}{
+		{
+			Name: "no-data",
+			Records: []*store.Record{},
+			Updated: "",
+			Status: "NO_DATA",
+			FirstRecord: frozenRecord{Status: "NO_DATA"},
+			LastRecord: frozenRecord{Status: "NO_DATA"},
+		},
+		{
+			Name: "single-failure",
+			Records: []*store.Record{&store.Record{
+				CheckedAt: time.Date(2021, time.January, 2, 20, 1, 2, 0, time.UTC),
+				Target: &url.URL{Scheme: "ping", Opaque: "local"},
+				Status: store.STATUS_FAILURE,
+				Message: "this is failure",
+				Latency: 654321 * time.Microsecond,
+			}},
+			Updated: "2021-01-02T20:01:02Z",
+			Status: "FAILURE",
+			FirstRecord: frozenRecord{Status: "NO_DATA"},
+			LastRecord: frozenRecord{
+				CheckedAt: "2021-01-02T20:01:02Z",
+				Status: "FAILURE",
+				Message: "this is failure",
+				Latency: 654.321,
+			},
+		},
+		{
+			Name: "filled-unknown",
+			Records: append(filledRecords, &store.Record{
+				CheckedAt: time.Date(2021, time.January, 2, 17, 4, 3, 0, time.UTC),
+				Target: &url.URL{Scheme: "ping", Opaque: "local"},
+				Status: store.STATUS_UNKNOWN,
+				Message: "this is unknown",
+				Latency: 123321 * time.Microsecond,
+			}),
+			Updated: "2021-01-02T17:04:03Z",
+			Status: "UNKNOWN",
+			FirstRecord: frozenRecord{
+				CheckedAt: "2021-01-02T15:04:05Z",
+				Status: "HEALTHY",
+				Message: "filled",
+				Latency: 123.456,
+			},
+			LastRecord: frozenRecord{
+				CheckedAt: "2021-01-02T17:04:03Z",
+				Status: "UNKNOWN",
+				Message: "this is unknown",
+				Latency: 123.321,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.Name, func(t *testing.T) {
+			hs := &store.ProbeHistory{
+				Target: &url.URL{Scheme: "ping", Opaque: "localhost"},
+				Records: tt.Records,
+			}
+
+			frozen := freezeProbeHistory(hs)
+
+			if frozen.Target != "ping:localhost" {
+				t.Errorf("unexpected target: %s", frozen.Target)
+			}
+
+			if frozen.Status != tt.Status {
+				t.Errorf("unexpected status: %s", frozen.Status)
+			}
+
+			if frozen.Updated != tt.Updated {
+				t.Errorf("unexpected updated: %s", frozen.Updated)
+			}
+
+			if len(frozen.History) != store.PROBE_HISTORY_LEN {
+				t.Errorf("unexpected number of history: %d", len(frozen.History))
+			}
+
+			if !reflect.DeepEqual(frozen.History[0], tt.FirstRecord) {
+				t.Errorf("unexpected first record: %#v", frozen.History[0])
+			}
+
+			if !reflect.DeepEqual(frozen.History[len(frozen.History)-1], tt.LastRecord) {
+				t.Errorf("unexpected last record: %#v", frozen.History[len(frozen.History)-1])
+			}
+		})
+	}
+}
