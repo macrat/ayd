07a03f4f0106e1e81047ff35d15422536938b363
MacRat <m@crat.jp>
2021-04-24 18:04:47 +0900

    test: fix test of exec for windows

--- probe/exec_windows_test.go
+++ probe/exec_windows_test.go
@@ -14,7 +14,7 @@ func TestExecuteProbe(t *testing.T) {
 	AssertProbe(t, []ProbeTest{
 		{`exec:stub\test.bat?message=hello&code=0`, store.STATUS_HEALTHY, "hello"},
 		{`exec:stub\test.bat?message=world&code=1`, store.STATUS_FAILURE, "world"},
-		{"exec:echo#%0D%0Ahello%0D%0Aworld%0D%0A%0D%0A", store.STATUS_HEALTHY, "hello\nworld"},
+		{"exec:echo#%0Ahello%0Aworld%0A%0A", store.STATUS_HEALTHY, "hello\nworld"},
 		{`exec:stub\no-such-script`, store.STATUS_UNKNOWN, `exec: "stub\\\\no-such-script": file does not exist`},
 		{"exec:no-such-command", store.STATUS_UNKNOWN, `exec: "no-such-command": executable file not found in %PATH%`},
 		{"exec:sleep#10", store.STATUS_UNKNOWN, `timeout`},
