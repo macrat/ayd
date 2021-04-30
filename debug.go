b4b2059ba9dfff5e76c38af5f26b01b7b0cb06b8
MacRat <m@crat.jp>
2021-04-30 21:28:53 +0900

    test: add debug build mode for memory leak test

---
+++ debug.go
@@ -0,0 +1,31 @@
+// +build debug
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+)
+
+func debugStatusReporter() {
+	f, err := os.OpenFile("ayd_debug.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "DEBUG: failed to open debug log file: %s\n", err)
+		return
+	}
+	fmt.Fprintf(f, "timestamp\tcurrent_goroutines\tcurrent_heap_bytes\tnum_mallocs\tnum_frees\n")
+
+	var mem runtime.MemStats
+
+	t := time.Tick(5 * time.Second)
+	for range t {
+		runtime.ReadMemStats(&mem)
+		fmt.Fprintf(f, "%s\t%d\t%d\t%d\t%d\n", time.Now().Format(time.RFC3339), runtime.NumGoroutine(), mem.HeapAlloc, mem.Mallocs, mem.Frees)
+	}
+}
+
+func init() {
+	go debugStatusReporter()
+}
