99cb68cfc7d73712d26a7d7457c99e1ef6892671
MacRat <m@crat.jp>
2021-05-01 20:04:35 +0900

    feat: add cors header for json endpoint

--- exporter/json.go
+++ exporter/json.go
@@ -10,6 +10,8 @@ import (
 func JSONExporter(s *store.Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.Header().Set("Access-Controll-Allow-Origin", "*")
+		w.Header().Set("Access-Controll-Allow-Methods", "GET")
 
 		enc := json.NewEncoder(w)
 		enc.SetIndent("", "  ")
