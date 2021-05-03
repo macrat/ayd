a59b11a8c9c2db2c7b7f4c476d044ab045391a68
MacRat <m@crat.jp>
2021-05-03 18:02:12 +0900

    test: fix exec test for windows

--- probe/exec_windows_test.go
+++ probe/exec_windows_test.go
@@ -3,6 +3,7 @@
 package probe_test
 
 import (
+	"net/url"
 	"testing"
 
 	"github.com/macrat/ayd/probe"
@@ -16,7 +17,7 @@ func TestExecuteProbe(t *testing.T) {
 		{`exec:./testdata/test.bat?message=hello&code=0`, store.STATUS_HEALTHY, "hello", ""},
 		{`exec:./testdata/test.bat?message=world&code=1`, store.STATUS_FAILURE, "world", ""},
 		{"exec:echo#%0Ahello%0Aworld%0A%0A", store.STATUS_HEALTHY, "hello\nworld", ""},
-		{"exec:./testdata/no-such-script", store.STATUS_UNKNOWN, ``, `exec: ".\testdata\no-such-script": stat .\testdata\no-such-script: no such file or directory`},
+		{"exec:./testdata/no-such-script", store.STATUS_UNKNOWN, ``, `exec: ".\\\\testdata\\\\no-such-script": file does not exist`},
 		{"exec:no-such-command", store.STATUS_UNKNOWN, ``, `exec: "no-such-command": executable file not found in %PATH%`},
 		{"exec:sleep#10", store.STATUS_UNKNOWN, `probe timed out`, ""},
 		{"exec:echo#::status::unknown", store.STATUS_UNKNOWN, ``, ""},
@@ -25,8 +26,8 @@ func TestExecuteProbe(t *testing.T) {
 
 	AssertTimeout(t, "exec:echo")
 
-	t.Func("normalize-path", func(t *testing.T) {
-		tests := struct {
+	t.Run("normalize-path", func(t *testing.T) {
+		tests := []struct {
 			From string
 			To   string
 		}{
@@ -35,7 +36,7 @@ func TestExecuteProbe(t *testing.T) {
 		}
 
 		for _, tt := range tests {
-			p, err := probe.NewExecProbe(&url.URL{Scheme: "exec", Opaque: tt.From})
+			p, err := probe.NewExecuteProbe(&url.URL{Scheme: "exec", Opaque: tt.From})
 			if err != nil {
 				t.Errorf("%s: failed to create probe: %s", tt.From, err)
 			}
