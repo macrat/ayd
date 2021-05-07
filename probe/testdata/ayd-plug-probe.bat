345bbc46a2810ffe0bb1f926290d07a7caaac661
MacRat <m@crat.jp>
2021-05-07 23:46:24 +0900

    test: fix test plugin on windows

--- probe/testdata/ayd-plug-probe.bat
+++ probe/testdata/ayd-plug-probe.bat
@@ -1,9 +1,9 @@
 @echo off
 
-if %1 == plug:empty (
+if not "%1" == "plug:empty" (
     echo 2001-02-03T16:05:06Z	HEALTHY	123.456	%1	check %1
 )
 
-if %1 == plug:invalid-record (
+if "%1" == "plug:invalid-record" (
     echo this is invalid
 )
