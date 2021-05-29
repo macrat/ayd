b544651bc9f70161c4d04f38e3f858c448980a93
MacRat <m@crat.jp>
2021-05-30 01:54:03 +0900

    test: add gc_num column debug log

--- debug.go
+++ debug.go
@@ -15,14 +15,14 @@ func debugStatusReporter() {
 		fmt.Fprintf(os.Stderr, "DEBUG: failed to open debug log file: %s\n", err)
 		return
 	}
-	fmt.Fprintf(f, "timestamp\tcurrent_goroutines\tcurrent_heap_bytes\tnum_mallocs\tnum_frees\n")
+	fmt.Fprintf(f, "timestamp\tcurrent_goroutines\tcurrent_heap_bytes\tnum_mallocs\tnum_frees\tnum_gc\n")
 
 	var mem runtime.MemStats
 
 	t := time.Tick(5 * time.Second)
 	for range t {
 		runtime.ReadMemStats(&mem)
-		fmt.Fprintf(f, "%s\t%d\t%d\t%d\t%d\n", time.Now().Format(time.RFC3339), runtime.NumGoroutine(), mem.HeapAlloc, mem.Mallocs, mem.Frees)
+		fmt.Fprintf(f, "%s\t%d\t%d\t%d\t%d\t%d\n", time.Now().Format(time.RFC3339), runtime.NumGoroutine(), mem.HeapAlloc, mem.Mallocs, mem.Frees, mem.NumGC)
 	}
 }
 
