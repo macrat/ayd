d7e336c1f0c0174b2930846f69f32e3e016ead67
MacRat <m@crat.jp>
2021-06-07 21:23:29 +0900

    test: add test for exec error when run time

--- probe/exec_test.go
+++ probe/exec_test.go
@@ -2,11 +2,55 @@ package probe_test
 
 import (
 	"context"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 	"testing"
 	"time"
 
+	api "github.com/macrat/ayd/lib-ayd"
 	"github.com/macrat/ayd/testutil"
 )
+
+func TestExecuteProbe_unknownError(t *testing.T) {
+	dir := t.TempDir()
+	file := filepath.Join(dir, "test.bat")
+
+	f, err := os.Create(file)
+	if err != nil {
+		t.Fatalf("failed to create test file: %s", err)
+	}
+	if err := f.Chmod(0766); err != nil {
+		t.Fatalf("failed to change permission of test file: %s", err)
+	}
+	f.Close()
+
+	p := testutil.NewProbe(t, "exec:"+file)
+
+	if err := os.Chmod(file, 0000); err != nil {
+		t.Fatalf("failed to change permission of test file: %s", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+
+	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		rs := testutil.RunCheck(ctx, p)
+		if rs[0].Status != api.StatusUnknown || !strings.Contains(rs[0].Message, "permission denied") {
+			t.Errorf("unexpected result:\n%s", rs[0])
+		}
+	}
+
+	if err := os.Remove(file); err != nil {
+		t.Fatalf("failed to remove test file: %s", err)
+	}
+
+	rs := testutil.RunCheck(ctx, p)
+	if rs[0].Status != api.StatusUnknown || (!strings.Contains(rs[0].Message, "no such file or directory") && !strings.Contains(rs[0].Message, "file does not exist")) {
+		t.Errorf("unexpected result:\n%s", rs[0])
+	}
+}
 
 func BenchmarkExecuteProbe(b *testing.B) {
 	p := testutil.NewProbe(b, "exec:echo#hello-world")
