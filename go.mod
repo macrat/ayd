18393aa9340361d2dd5c48cca2371c6b62c155d2
MacRat <m@crat.jp>
2021-10-07 20:04:22 +0900

    test(endpoint): add test of status pages

--- go.mod
+++ go.mod
@@ -10,3 +10,5 @@ require (
 	golang.org/x/sys v0.0.0-20210925032602-92d5a993a665
 	golang.org/x/text v0.3.6
 )
+
+require github.com/google/go-cmp v0.5.6 // indirect
--- internal/exporter/status_test.go
+++ internal/exporter/status_test.go
@@ -2,12 +2,32 @@ package exporter_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"regexp"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/macrat/ayd/internal/testutil"
 )
+
+func readTestFile(t *testing.T, file string) string {
+	t.Helper()
+
+	f, err := os.Open(file)
+	if err != nil {
+		t.Fatalf("failed to open file: %s", err)
+	}
+
+	bs, err := io.ReadAll(f)
+	if err != nil {
+		t.Fatalf("failed to read file: %s", err)
+	}
+
+	return string(bs)
+}
 
 func TestStatusHTMLExporter(t *testing.T) {
 	srv := testutil.StartTestServer(t)
@@ -19,10 +39,25 @@ func TestStatusHTMLExporter(t *testing.T) {
 		t.Errorf("unexpected status: %s", resp.Status)
 	}
 
-	if resp, err := srv.Client().Get(srv.URL + "/"); err != nil {
+	resp, err := srv.Client().Get(srv.URL + "/")
+
+	if err != nil {
 		t.Errorf("failed to get /: %s", err)
-	} else if resp.StatusCode != http.StatusOK {
+	}
+	if resp.StatusCode != http.StatusOK {
 		t.Errorf("unexpected status: %s", resp.Status)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed to read body: %s", err)
+	}
+
+	result := string(regexp.MustCompile(`Reported By Ayd\? \(.+\)`).ReplaceAll(body, []byte("Reported By Ayd? (-- CURRENT TIME MASKED --)")))
+
+	if diff := cmp.Diff(result, readTestFile(t, "./testdata/status.html")); diff != "" {
+		t.Errorf(diff)
 	}
 }
 
@@ -30,19 +65,38 @@ func TestStatusTextExporter(t *testing.T) {
 	srv := testutil.StartTestServer(t)
 	defer srv.Close()
 
-	tests := []string{
-		"/status.txt",
-		"/status.txt?charset=unicode",
-		"/status.txt?charset=ascii",
+	tests := []struct {
+		URL  string
+		File string
+	}{
+		{"/status.txt", "./testdata/status.unicode.txt"},
+		{"/status.txt?charset=unicode", "./testdata/status.unicode.txt"},
+		{"/status.txt?charset=ascii", "./testdata/status.ascii.txt"},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt, func(t *testing.T) {
-			u := srv.URL + tt
-			if resp, err := srv.Client().Get(u); err != nil {
+		t.Run(tt.URL, func(t *testing.T) {
+			u := srv.URL + tt.URL
+			resp, err := srv.Client().Get(u)
+
+			if err != nil {
 				t.Errorf("failed to get %s: %s", u, err)
-			} else if resp.StatusCode != http.StatusOK {
+			}
+			if resp.StatusCode != http.StatusOK {
 				t.Errorf("unexpected status: %s", resp.Status)
+			}
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("failed to read body: %s", err)
+			}
+
+			result := string(regexp.MustCompile(`Reported by Ayd\? \(.+\)`).ReplaceAll(body, []byte("Reported by Ayd? (-- CURRENT TIME MASKED --)")))
+
+			if diff := cmp.Diff(result, readTestFile(t, fmt.Sprintf(tt.File))); diff != "" {
+				t.Log(result)
+				t.Errorf(diff)
 			}
 		})
 	}
