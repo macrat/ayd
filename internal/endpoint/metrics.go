ef1422b1490e052cd2d28104bf51cb3992d0ab1b
MacRat <m@crat.jp>
2021-10-23 15:40:40 +0900

    feat(endpoint)!: remake /metrics endpoint
    ayd_healthy metric has renamed to ayd_status{status="healthy"}, and now
    you can detect other status for example failure.
    
    the bug that the timestamp format is incorrect has fixed in this commit.

--- internal/endpoint/metrics.go
+++ internal/endpoint/metrics.go
@@ -9,42 +9,79 @@ import (
 	api "github.com/macrat/ayd/lib-ayd"
 )
 
+// metricInfo is a metric point for /metrics endpoint.
+type metricInfo struct {
+	Timestamp int64
+	Target    string
+	Healthy   int
+	Unknown   int
+	Failure   int
+	Aborted   int
+	Latency   float64
+}
+
+// MetricsEndpoint implements Prometheus metrics endpoint.
+// This endpoint follows both of Prometheus specification and OpenMetrics specification.
 func MetricsEndpoint(s *store.Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		for _, hs := range s.ProbeHistory() {
+		hss := s.ProbeHistory()
+		metrics := make([]metricInfo, 0, len(hss))
+		for _, hs := range hss {
 			if len(hs.Records) > 0 {
 				last := hs.Records[len(hs.Records)-1]
 
-				up := 0
-				if last.Status == api.StatusHealthy {
-					up = 1
+				m := metricInfo{
+					Timestamp: last.CheckedAt.UnixMilli(),
+					Latency:   last.Latency.Seconds(),
+					Target:    strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(hs.Target.String(), "\\", "\\\\"), "\n", "\\\n"), "\"", "\\\""),
 				}
-				latency := last.Latency.Seconds()
-				target := strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(hs.Target.String(), "\\", "\\\\"), "\n", "\\\n"), "\"", "\\\"")
 
-				timestamp := last.CheckedAt.Unix()
+				switch last.Status {
+				case api.StatusHealthy:
+					m.Healthy = 1
+				case api.StatusUnknown:
+					m.Unknown = 1
+				case api.StatusFailure:
+					m.Failure = 1
+				case api.StatusAborted:
+					m.Aborted = 1
+				}
 
-				fmt.Fprintln(w, "# HELP ayd_healthy 1 if target is healthy, otherwise 0.")
-				fmt.Fprintln(w, "# TYPE ayd_healthy Gauge")
-				fmt.Fprintf(w, "ayd_healthy{target=\"%s\"} %d %d\n", target, up, timestamp)
-				fmt.Fprintln(w, "# HELP ayd_latency_seconds A duration in seconds that taken checking for the target.")
-				fmt.Fprintln(w, "# TYPE ayd_latency_seconds Gauge")
-				fmt.Fprintf(w, "ayd_latency_seconds{target=\"%s\"} %f %d\n", hs.Target, latency, timestamp)
-				fmt.Fprintln(w)
+				metrics = append(metrics, m)
 			}
 		}
 
-		fmt.Fprintln(w, "# HELP ayd_healthy 1 if target is healthy, otherwise 0.")
-		fmt.Fprintln(w, "# TYPE ayd_healthy Gauge")
-		if failing, _ := s.Errors(); failing {
-			fmt.Fprintln(w, `ayd_healthy{target="ayd"} 0`)
-		} else {
-			fmt.Fprintln(w, `ayd_healthy{target="ayd"} 1`)
+		fmt.Fprintln(w, "# HELP ayd_status The target status.")
+		fmt.Fprintln(w, "# TYPE ayd_status gauge")
+		for _, m := range metrics {
+			fmt.Fprintf(w, "ayd_status{target=\"%s\",status=\"healthy\"} %d %d\n", m.Target, m.Healthy, m.Timestamp)
+			fmt.Fprintf(w, "ayd_status{target=\"%s\",status=\"unknown\"} %d %d\n", m.Target, m.Unknown, m.Timestamp)
+			fmt.Fprintf(w, "ayd_status{target=\"%s\",status=\"failure\"} %d %d\n", m.Target, m.Failure, m.Timestamp)
+			fmt.Fprintf(w, "ayd_status{target=\"%s\",status=\"aborted\"} %d %d\n", m.Target, m.Aborted, m.Timestamp)
 		}
-
+		if healthy, _ := s.Errors(); healthy {
+			fmt.Fprintln(w, `ayd_status{target="ayd",status="healthy"} 1`)
+			fmt.Fprintln(w, `ayd_status{target="ayd",status="unknown"} 0`)
+			fmt.Fprintln(w, `ayd_status{target="ayd",status="failure"} 0`)
+			fmt.Fprintln(w, `ayd_status{target="ayd",status="aborted"} 0`)
+		} else {
+			fmt.Fprintln(w, `ayd_status{target="ayd",status="healthy"} 0`)
+			fmt.Fprintln(w, `ayd_status{target="ayd",status="unknown"} 0`)
+			fmt.Fprintln(w, `ayd_status{target="ayd",status="failure"} 1`)
+			fmt.Fprintln(w, `ayd_status{target="ayd",status="aborted"} 0`)
+		}
 		fmt.Fprintln(w)
-		fmt.Fprintln(w, "# HELP ayd_incident_count The number of incident happened since server started")
-		fmt.Fprintln(w, "# TYPE ayd_incident_count Counter")
-		fmt.Fprintf(w, "ayd_incident_count %d\n", s.IncidentCount)
+
+		fmt.Fprintln(w, "# HELP ayd_latency_seconds The duration in seconds that taken checking for the target.")
+		fmt.Fprintln(w, "# TYPE ayd_latency_seconds gauge")
+		fmt.Fprintln(w, "# UNIT ayd_latency_seconds seconds")
+		for _, m := range metrics {
+			fmt.Fprintf(w, "ayd_latency_seconds{target=\"%s\"} %f %d\n", m.Target, m.Latency, m.Timestamp)
+		}
+		fmt.Fprintln(w)
+
+		fmt.Fprintln(w, "# HELP ayd_incident_total The number of incident happened since server started.")
+		fmt.Fprintln(w, "# TYPE ayd_incident_total counter")
+		fmt.Fprintf(w, "ayd_incident_total %d\n", s.IncidentCount)
 	}
 }
