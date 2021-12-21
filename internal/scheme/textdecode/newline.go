bbef5b0b9c49251f38eb27ebd064aaea6f365989
MacRat <m@crat.jp>
2021-12-21 20:56:16 +0900

    feat: optimize newline normalizer

--- internal/scheme/textdecode/newline.go
+++ internal/scheme/textdecode/newline.go
@@ -9,29 +9,45 @@ import (
 type newlineNormalizer struct{}
 
 func (nn newlineNormalizer) Transform(dst, src []byte, atEOF bool) (nDst, nSrc int, err error) {
+	if len(dst) < len(src) {
+		return 0, 0, transform.ErrShortDst
+	}
+	copy(dst, src)
+
 	nSrc = bytes.IndexByte(src, byte(0))
+	end := nSrc
 	if nSrc < 0 {
 		nSrc = len(src)
+		end = nSrc - 1
 	}
-	if nSrc == 0 {
+
+	initial := bytes.IndexByte(src, byte('\r'))
+	if initial < 0 {
+		nDst = nSrc
 		return
 	}
 
-	x := src[:nSrc]
-	x = bytes.ReplaceAll(x, []byte("\r\n"), []byte("\n"))
-	x = bytes.ReplaceAll(x, []byte("\r"), []byte("\n"))
-
-	if len(x) > len(dst) {
-		return 0, 0, transform.ErrShortDst
+	shift := 0
+	for i := initial; i+shift < end; i++ {
+		if dst[i+shift] == byte('\r') {
+			if dst[i+shift+1] == byte('\n') {
+				shift++
+			} else {
+				dst[i+shift] = byte('\n')
+			}
+		}
+		dst[i] = dst[i+shift]
 	}
 
-	copy(dst, x)
-	nDst = len(x)
-
-	if !atEOF && src[nSrc-1] == byte('\r') {
-		nSrc--
-		nDst--
+	if src[len(src)-1] == byte('\r') {
+		if atEOF {
+			dst[nSrc-shift-1] = byte('\n')
+		} else {
+			nSrc--
+		}
 	}
+
+	nDst = nSrc - shift
 
 	return
 }
